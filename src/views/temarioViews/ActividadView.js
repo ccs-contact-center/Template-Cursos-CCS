7a7184a1dca23efab7675bf7716852dc2a6cb432
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-10 16:01:29 -0500

    Update ActividadView.js

--- src/views/temarioViews/ActividadView.js
+++ src/views/temarioViews/ActividadView.js
@@ -1,40 +1,28 @@
-import React, { Component } from 'react';
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row
-} from 'reactstrap';
+import React, { Component } from "react";
+import { CardBody, CardHeader, Col, Row } from "reactstrap";
 
-import List2 from './componentsExtras/dragdrog';
+import List2 from "./componentsExtras/dragdrog";
 
 class ActividadView extends Component {
-
-
-  
-  loading = () => <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+  loading = () => (
+    <div className="animated fadeIn pt-1 text-center">Cargando...</div>
+  );
 
   render() {
-
     return (
       <div className="animated fadeIn">
-
         <Row>
           <Col>
-            
-              <CardHeader className="">
-               <h2> Actividad</h2>
-              </CardHeader>
-              <CardBody className="text-center">
-             <List2></List2>
-              </CardBody>
-          
+            <CardHeader className="">
+              <h2> Actividad</h2>
+            </CardHeader>
+            <CardBody className="text-center">
+              <List2></List2>
+            </CardBody>
           </Col>
         </Row>
       </div>
     );
   }
 }
-export default ActividadView
-
+export default ActividadView;
