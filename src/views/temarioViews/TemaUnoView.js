b22edee82572d604d0429c19c74ae6c1b7b70b4d
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-10 16:01:54 -0500

    Update TemaUnoView.js

--- src/views/temarioViews/TemaUnoView.js
+++ src/views/temarioViews/TemaUnoView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, CardBody, CardHeader, Col, Row } from 'reactstrap'
+import {  CardBody, CardHeader, Col, Row } from 'reactstrap'
 import Carousel from 'react-bootstrap/Carousel'
 import Nota from './componentsExtras/notasReferencia'
 
