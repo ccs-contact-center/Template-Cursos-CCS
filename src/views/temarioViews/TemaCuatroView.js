02ad2ee327d7bcc3fc03c0d280d60e2d8bc4aa78
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-10 16:01:40 -0500

    Update TemaCuatroView.js

--- src/views/temarioViews/TemaCuatroView.js
+++ src/views/temarioViews/TemaCuatroView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, CardBody, CardHeader, Col, Row } from 'reactstrap'
+import {  CardBody, CardHeader, Col, Row } from 'reactstrap'
 import {
   TablaLineaHistorico,
   TablasImputables,
